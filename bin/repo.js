4fc6f6e832c224e1a3d6f25deab91e6b59d43330
gxc <1003823149@qq.com>
2022-10-27 14:49:29 +0800

    feat:add new branch

---
+++ bin/repo.js
@@ -0,0 +1,12 @@
+//模板下载地址
+exports.config = {
+  "vue3":{
+    url:"/template/vue3",
+    bootstrap:"npm run serve"
+  },
+  "vue":{
+    url:"/template/vue3",
+    bootstrap:"yarn serve",
+    install:"yarn install"
+  }
+}
